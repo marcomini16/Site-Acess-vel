3dfefca8dc889ae8d464d0cd4b3a9ef886fc9a25
marcomini16 <00001110748644sp@al.educacao.sp.gov.br>
2025-08-28 15:33:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ document.getElementById('opcoesacessibilidade');
  botaoDeAcessibilidade.addEventListener('click', () => {
  botaoDeAcessibilidade.classList.toggle('rotacao-botao');
  opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+ botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
 })
 alternaContraste.addEventListener('click', function(){
 document.body.classList.toggle('alto-contraste');
